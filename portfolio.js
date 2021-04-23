56dad9ad4ca41bb3b4be525dd8608f44121efb20
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-23 15:02:05 -0300

    parar um pokinho

---
+++ portfolio.js
@@ -0,0 +1,63 @@
+const portfolioImages = document.getElementsByClassName('port');
+const portfolioType = document.querySelector('.portfolio-type ul');
+const graphicDesign = [
+	'Preset_Bíbilico_4.jpg',
+	'Preset_Biblico_3.jpg',
+	'Preset_Bíblico_6_6.jpg',
+	'Preset_Bíblico_7.jpg',
+	'Preset_Bíblico_8.jpg',
+	'Preset_Bíblico_10.jpg',
+];
+const webDesign = [
+	'site_1.png',
+	'site_2.png',
+	'site_3.png',
+	'site_4.png',
+	'site_1.png',
+	'site_2.png',
+];
+
+function sorteioWebGraphic() {
+	const number = Math.floor(Math.random() * 2);
+	if(number === 1){
+		return graphicDesign;
+	} else {
+		return webDesign;
+	}
+}
+
+function selectedPortfolio() {
+	const selected = document.querySelector('.selected');
+	if (selected.innerText === 'All') {
+		for (let index = 0; index < portfolioImages.length; index += 1){
+			const array = sorteioWebGraphic();
+			portfolioImages[index].style.backgroundImage = `url(${array[index]})`;
+		}
+	} else if (selected.innerText === 'Web Design') {
+		for (let index = 0; index < portfolioImages.length; index += 1){
+			portfolioImages[index].style.backgroundImage = `url(${webDesign[index]})`;
+		}
+	}	else {
+		for (let index = 0; index < portfolioImages.length; index += 1){
+			portfolioImages[index].style.backgroundImage = `url(${graphicDesign[index]})`;
+		}
+	}
+}
+
+function selectPortfolio(element) {
+	if (element.target.parentNode === portfolioType) {
+    if (document.querySelector('.selected') !== null) {
+      document.querySelector('.selected').classList.remove('selected');
+      element.target.classList.add('selected');
+    } else {
+      element.target.classList.add('selected');
+    }
+  }
+	selectedPortfolio();
+}
+
+window.onload = function() {
+	selectedPortfolio();
+}
+
+addEventListener('click', selectPortfolio);
